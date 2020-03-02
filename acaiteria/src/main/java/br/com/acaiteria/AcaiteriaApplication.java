ba18004ce705e952ac3560859ee263ed61103461
Rafael Alves <rafaell.alves.dev@gmail.com>
2020-03-02 08:22:18 -0300

    iniciando projeto

---
+++ acaiteria/src/main/java/br/com/acaiteria/AcaiteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.acaiteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaiteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaiteriaApplication.class, args);
+	}
+
+}
